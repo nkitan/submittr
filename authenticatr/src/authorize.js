7e2a45bb545b08bfc7ed5c445a8e3f56e10e03c9
nkitan <ankitdas2k@gmail.com>
2022-01-09 19:02:20 +0530

    QOL improvements

--- authenticatr/src/authorize.js
+++ authenticatr/src/authorize.js
@@ -10,38 +10,44 @@ module.exports = async (req, res, next) => {
             return res.status(400).json({
                 "message": "no token found"
             })
-        }
-
+        } else {
         // TODO fix error when cannot refresh more than once
-        try {
-            const payload = await jwt.verify(req.cookies.token, process.env.JWT_SECRET)
-            req.id = payload.id;
-            
             try {
-                req.isAdmin = Object.values((await userpool.query("SELECT isadmin FROM users WHERE id = $1", [payload.id])).rows[0])
-                req.isTeacher = Object.values((await userpool.query("SELECT isteacher FROM users WHERE id = $1", [payload.id])).rows[0])
-                req.username = Object.values((await userpool.query("SELECT username FROM users WHERE id = $1", [payload.id])).rows[0])
-                next();
-            }
+                const payload = await jwt.verify(req.cookies.token, process.env.JWT_SECRET)
 
-            catch(err){
-                logger.error(err.message)
-                return res.status(500).json({
-                    message: 'database error'
+                if(payload === null){
+                    throw new Error('failed to verify, could not parse payload')
+                }
+
+                try {
+                    req.id = payload.id;
+                    req.isAdmin = Object.values((await userpool.query("SELECT isadmin FROM users WHERE id = $1", [payload.id])).rows[0])
+                    req.isTeacher = Object.values((await userpool.query("SELECT isteacher FROM users WHERE id = $1", [payload.id])).rows[0])
+                    req.username = Object.values((await userpool.query("SELECT username FROM users WHERE id = $1", [payload.id])).rows[0])
+                    next();
+                }
+
+                catch(error){
+                    logger.error(error.message)
+                    return res.status(500).json({
+                        message: 'database error'
+                    })
+                }
+
+            } catch (error) {
+                logger.error(error.message)
+                return res.status(401).json({
+                    message: "couldn't refresh"
                 })
             }
-
-        } catch (error) {
-            logger.error(error.message)
-            return res.status(401).json({
-                message: "couldn't refresh"
-            })
         }
-    } catch (err) {
-        logger.error(err.message);
+            
+    } catch (error) {
+        logger.error(error.message);
         return res.status(401).json({
             valid: false,
             message: 'invalid token'
         });
     }
 }
+
